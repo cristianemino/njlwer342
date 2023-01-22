e962d305c6a945645748d3e68862c3ec903b3848
cristianmino <cristianeisasa@gmail.com>
2023-01-21 22:06:35 -0300

    Resolviendo el ejercicio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, useState  } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
+const App = () => {
+
+const [tasks, setTasks] = useState([]);
+
+const addTask = (task) => {
+    setTasks([...tasks, task]);
+    document.getElementById("new-task").value = ""
+  };
+
     return (
       <div className="wrapper">
         <div className="list">
@@ -12,15 +19,15 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {tasks.map((task, index) => (
+              <li key={index}>{task}</li>
+            ))}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" onKeyDown={(e) => e.key === 'Enter' && addTask(e.target.value)} />
         </div>
       </div>
     )
   }
-}
 
 
 export default App;
